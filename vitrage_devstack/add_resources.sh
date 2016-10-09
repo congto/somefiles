f78f3d27f9ad56eecc4d65fa618e4d4178c89a36
Idan Hefetz <idan.hefetz@nokia.com>
2016-10-09 13:10:33 +0000

    devstack files

--- vitrage_devstack/add_resources.sh
+++ vitrage_devstack/add_resources.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-source /home/stack/keystonerc
+cd /home/stack
+. /home/stack/keystonerc
 
-#Images
-wget https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/tinylinux.qcow2
+echo "Creating Image"
+if [ ! -f /home/stack/tinylinux.qcow2 ]; then
+    wget https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/tinylinux.qcow2
+fi
 openstack image create TinyLinux --public --file tinylinux.qcow2
 
-#Networks
+echo "Creating Network & Subnet"
 openstack network create NetA --share
 openstack subnet create subnetA --network NetA --subnet-range 8.8.8.0/24 --gateway 8.8.8.1
 
-#Heat Stack
-#wget https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
+echo "Creating Heat Stack"
+openstack stack create some_stack --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
 
