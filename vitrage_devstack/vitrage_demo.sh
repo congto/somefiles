b464dd7d349ba317faeeea55c3acfa92aa836c6b
Idan Hefetz <idan.hefetz@nokia.com>
2016-10-16 09:34:19 +0000

    devstack files

---
+++ vitrage_devstack/vitrage_demo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "vitrage_demo.sh: Running stack.sh"
+cd /home/stack/devstack
+./stack.sh
+
+echo "vitrage_demo.sh: Starting Nagios"
+sudo omd start my_site
+sudo service xinetd restart
+
+echo "vitrage_demo.sh: Configuring Devstack"
+. /home/stack/keystonerc
+
+echo "vitrage_demo.sh: Creating Image"
+openstack image create TinyLinux --public --file /home/stack/tinylinux.qcow2
+
+echo "vitrage_demo.sh: Creating Network & Subnet"
+openstack network create NetA --share
+openstack subnet create subnetA --network NetA --subnet-range 8.8.8.0/24 --gateway 8.8.8.1
+
+echo "vitrage_demo.sh: Creating Heat Stack"
+openstack stack create some_stack --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
