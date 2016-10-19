63700bba9ba0b17811e3501e24947fbe33d0bc59
Idan Hefetz <idan.hefetz@nokia.com>
2016-10-19 09:18:14 +0000

    devstack files

--- vitrage_devstack/vitrage_demo.sh
+++ vitrage_devstack/vitrage_demo.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
+# Close STDOUT file descriptor
+exec 1<&-
+# Close STDERR FD
+exec 2<&-
+# Open STDOUT as $LOG_FILE file for read and write.
+exec 1<>/tmp/vitrage_demo.log
+# Redirect STDERR to STDOUT
+exec 2>&1
+
 echo "Starting zabbix"
 sudo service zabbix-server start
 sudo service zabbix-agent start
 
-echo "vitrage_demo.sh: Running stack.sh"
+echo "vitrage_demo.sh: Change to user stack"
+sudo su - stack
+cd ~
+
+echo "vitrage_demo.sh: Getting local.conf"
 cd /home/stack/devstack
+mv local.conf local.conf.orig
+wget https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/local.conf
+
+echo "vitrage_demo.sh: Running stack.sh"
 ./stack.sh
 
 echo "vitrage_demo.sh: Configuring Devstack"
