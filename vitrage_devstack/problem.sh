282f9c354cd3248a86904c639d880268e309c0e7
Idan Hefetz <idan.hefetz@nokia.com>
2016-10-23 09:37:05 +0000

    devstack files

--- vitrage_devstack/problem.sh
+++ vitrage_devstack/problem.sh
@@ -9,16 +9,16 @@
 #Duplicate trigger  Lack of available memory on server {HOST.NAME} and change expression to "{compute-0-0:vm.memory.size[available].last(0)}<250M"
 #Disable the original trigger
 
-echo "What would you like to do?"
+echo "usage: ./problem.sh <option>"
 echo "c - Raise CPU levels            Compute-0-0      <cu undo>"
 echo "m - Raise Memory levels         Compute-0-0      <mu undo>"
 echo "i - ifdown public interface     Compute-0-0      <iu undo>"
 echo "s - Switch public port failure  Switch-0         <su undo>"
-echo "Enter your choice: []:"
 #read -r OPTION
 case $1 in
 
 c)  echo "Raise CPU levels on Compute-0-0"
+    pkill yes
     yes > /dev/null &
     yes > /dev/null &
     yes > /dev/null &
@@ -31,11 +31,13 @@ cu)  echo "Undo raise CPU levels on Compute-0-0"
     pkill yes
     ;;
 m)  echo  "Raise Memory levels on Compute-0-0"
+    pkill eatmemory
     eatmemory 300M &
     ;;
 mu) echo  "Undo raise Memory levels on Compute-0-0"
     pkill eatmemory
     ;;
+
 i)  echo  "ifdown public interface on Compute-0-0"
     kill 123123123
     ;;
