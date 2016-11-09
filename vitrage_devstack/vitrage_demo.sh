5c455ed4660e4c6cdef6ba1f345b509799a1967a
Idan Hefetz <Idandos@users.noreply.github.com>
2016-11-09 16:08:04 +0200

    Update vitrage_demo.sh

--- vitrage_devstack/vitrage_demo.sh
+++ vitrage_devstack/vitrage_demo.sh
@@ -19,13 +19,14 @@ cd /home/stack/devstack
 mv local.conf local.conf.orig
 wget https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/local.conf
 wget -O /etc/vitrage/zabbix_conf.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/zabbix_conf.yaml
-#wget -O /etc/vitrage/static_datasources/compute-0-1.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/static_datasources/compute-0-1.yaml
-#wget -O /etc/vitrage/static_datasources/compute-0-1-instances.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/static_datasources/compute-0-1-instances.yaml
+wget -O /etc/vitrage/static_datasources/compute-0-1.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/static_datasources/compute-0-1.yaml
+wget -O /etc/vitrage/static_datasources/compute-0-1-instances.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/static_datasources/compute-0-1-instances.yaml
 mkdir /home/stack/templates
-wget -O /home/stack/templates/host_high_cpu_load_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_high_cpu_load_scenarios.yaml
-wget -O /home/stack/templates/host_public_nic_failure_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_public_nic_failure_scenarios.yaml
-#wget -O /home/stack/templates/host_high_memory_load_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_high_memory_load_scenarios.yaml
-wget -O /home/stack/templates/stack_instance_problem.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/stack_instance_problem.yaml
+wget -O /etc/vitrage/templates/host_high_cpu_load_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_high_cpu_load_scenarios.yaml
+wget -O /etc/vitrage/templates/host_public_nic_failure_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_public_nic_failure_scenarios.yaml
+#wget -O /etc/vitrage/templates/host_high_memory_load_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/host_high_memory_load_scenarios.yaml
+wget -O /etc/vitrage/templates/host_public_nic_failure_scenarios.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/stack_instance_critical_problem.yaml
+wget -O /etc/vitrage/templates/stack_instance_problem.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/templates/stack_instance_problem.yaml
 wget -O /home/stack/heat_stack.yaml https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
 
 sudo hostname compute-0-0
@@ -45,4 +46,4 @@ openstack network create NetA --share
 openstack subnet create subnetA --network NetA --subnet-range 8.8.8.0/24 --gateway 8.8.8.1
 
 #echo "vitrage_demo.sh: Creating Heat Stack"
-#openstack stack create app --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
+openstack stack create app --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
