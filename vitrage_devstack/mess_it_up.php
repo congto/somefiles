6b1aeca5371f603db9073138c4aa18b19b377dcd
Idan Hefetz <Idandos@users.noreply.github.com>
2017-03-22 14:03:24 +0200

    Update mess_it_up.php

--- vitrage_devstack/mess_it_up.php
+++ vitrage_devstack/mess_it_up.php
@@ -7,14 +7,6 @@
     {
          exec('/home/stack/problem.sh cu');
     }
-    if (isset($_POST['m']))
-    {
-         exec('/home/stack/problem.sh m');
-    }
-    if (isset($_POST['mu']))
-    {
-         exec('/home/stack/problem.sh mu');
-    }
     if (isset($_POST['i']))
     {
          exec('/home/stack/problem.sh i');
@@ -22,14 +14,6 @@
     if (isset($_POST['iu']))
     {
          exec('/home/stack/problem.sh iu');
-    }
-    if (isset($_POST['s']))
-    {
-         exec('/home/stack/problem.sh s');
-    }
-    if (isset($_POST['su']))
-    {
-         exec('/home/stack/problem.sh su');
     }
 ?>
 <html>
@@ -46,15 +30,6 @@
         <button name="cu">Undo</button>
     </p>
     </form>
-    <h2>Memory</h2>
-    <form method="post">
-    <p>
-        <button name="m">Raise Memory levels</button>
-    </p>
-    <p>
-        <button name="mu">Undo</button>
-    </p>
-    </form>
     <h2>Interface</h2>
     <form method="post">
     <p>
@@ -62,15 +37,6 @@
     </p>
     <p>
         <button name="iu">Undo</button>
-    </p>
-    </form>
-    <h2>Switch</h2>
-    <form method="post">
-    <p>
-        <button name="s">Switch public port failure</button>
-    </p>
-    <p>
-        <button name="su">Undo</button>
     </p>
     </form>
 </body>
