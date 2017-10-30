3065d548ba12f765c8101bed8fbb9ae06ecdb3fb
Idan Hefetz <idan.hefetz@nokia.com>
2017-10-30 12:30:51 +0000

    adding mess it up

--- mess_it_up/copy_to_apache.sh
+++ mess_it_up/copy_to_apache.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+rm -rf /idan
 mkdir /idan
 cp -r $SCRIPT_DIR/* /idan/
 rm -rf /usr/share/openstack-dashboard/static/app/css
@@ -11,5 +12,7 @@ mkdir /usr/share/openstack-dashboard/static/app/js
 cp /idan/css/* /usr/share/openstack-dashboard/static/app/css
 cp /idan/js/* /usr/share/openstack-dashboard/static/app/js
 cp /idan/mess_it_up.php /usr/share/openstack-dashboard/static/app/
+setenforce 0
+
 
 
