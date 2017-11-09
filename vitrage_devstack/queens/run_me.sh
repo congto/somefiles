449aa081724eba3e4299711948968c61454995e9
Idan Hefetz <Idandos@users.noreply.github.com>
2017-11-09 14:21:05 +0200

    Update run_me.sh

--- vitrage_devstack/queens/run_me.sh
+++ vitrage_devstack/queens/run_me.sh
@@ -8,27 +8,31 @@ sudo hostname compute-0-0
 ./stack.sh
 
 cd ~
+wget -O /etc/vitrage/zabbix_conf.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/zabbix_conf.yaml
+
 rm -rf keystonerc_admin
 wget https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/queens/keystonerc_admin
-wget https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/tinylinux.qcow2
-wget -O /etc/vitrage/zabbix_conf.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/zabbix_conf.yaml
-wget -O /home/stack/heat_stack.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/some_stack.yaml
-wget https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/install_zabbix_on_devstack.sh
-
 source keystonerc_admin
 source /home/stack/devstack/openrc
+
 echo "source keystonerc_admin" >> .bashrc
 echo "source /home/stack/devstack/openrc" >> .bashrc
 
+wget https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/tinylinux.qcow2
 openstack image create TinyLinux --public --file /home/stack/tinylinux.qcow2
 openstack network create NetA --share
 openstack subnet create subnetA --network NetA --subnet-range 8.8.8.0/24 --gateway 8.8.8.1
+
+wget -O /home/stack/heat_stack.yaml https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/some_stack.yaml
 openstack stack create App_1 --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
 openstack stack create App_2 --template https://github.com/Idandos/somefiles/raw/master/vitrage_devstack/some_stack.yaml
 
 sed -i.bak 's/stacks:global_index": "rule:deny_everybody/stacks:global_index": "/g' /etc/heat/policy.json
+
+wget https://raw.githubusercontent.com/Idandos/somefiles/master/vitrage_devstack/install_zabbix_on_devstack.sh
 chmod +x install_zabbix_on_devstack.sh
 sudo ./install_zabbix_on_devstack.sh
+
 sudo service devstack@vitrage-graph restart
 sudo service apache2 restart
 
