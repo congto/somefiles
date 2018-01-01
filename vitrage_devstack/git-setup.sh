251470d26ba3f9a11208ed94fc4367718c57e2ca
Idan Hefetz <Idandos@users.noreply.github.com>
2018-01-01 09:55:02 +0200

    Update git-setup.sh

--- vitrage_devstack/git-setup.sh
+++ vitrage_devstack/git-setup.sh
@@ -3,8 +3,8 @@ git config --global user.name "Idan Hefetz"
 git config --global user.email idan.hefetz@nokia.com
 git config --global gitreview.scheme https
 git config --global gitreview.port 443
-git config --global remote.gerrit.url ssh://idan-hefetz@review.openstack.org:29418/openstack/vitrage.git
 git config --global remote.gerrit.fetch +refs/heads/*:refs/remotes/gerrit/*
+git config remote.gerrit.url ssh://idan-hefetz@review.openstack.org:29418/openstack/vitrage.git
 sudo apt-get install git-review
 cd /opt/stack/vitrage
 cat /home/stack/.ssh/id_rsa.pub  (AND COPY TO GERRIT)
