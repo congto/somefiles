f06807d576f57e5c1d97c5c11e0420d8a6fa88bb
Idan Hefetz <Idandos@users.noreply.github.com>
2018-05-10 16:13:55 +0300

    Update copy_to_apache.sh

--- mess_it_up/copy_to_apache.sh
+++ mess_it_up/copy_to_apache.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm -rf /idan
-mkdir /idan
-cp -r $SCRIPT_DIR/* /idan/
-rm -rf /usr/share/openstack-dashboard/static/app/css
-rm -rf /usr/share/openstack-dashboard/static/app/js
-rm -rf /usr/share/openstack-dashboard/static/app/mess_it_up.php
-mkdir /usr/share/openstack-dashboard/static/app/css
-mkdir /usr/share/openstack-dashboard/static/app/js
-cp /idan/css/* /usr/share/openstack-dashboard/static/app/css
-cp /idan/js/* /usr/share/openstack-dashboard/static/app/js
-cp /idan/mess_it_up.php /usr/share/openstack-dashboard/static/app/
-setenforce 0
-
-
-
+sudo -i
+mkdir -p /var/lib/config-data/puppet-generated/horizon/usr/share/openstack-dashboard/static/app/
+mkdir -p /var/lib/config-data/puppet-generated/horizon/root/mess_it_up
+cp $SCRIPT_DIR/mess_it_up.php /var/lib/config-data/puppet-generated/horizon/usr/share/openstack-dashboard/static/app/
+cp -r $SCRIPT_DIR/css /var/lib/config-data/puppet-generated/horizon/usr/share/openstack-dashboard/static/app/
+cp -r $SCRIPT_DIR/js /var/lib/config-data/puppet-generated/horizon/usr/share/openstack-dashboard/static/app/
+cp $SCRIPT_DIR/button_*.sh /var/lib/config-data/puppet-generated/horizon/root/mess_it_up/
