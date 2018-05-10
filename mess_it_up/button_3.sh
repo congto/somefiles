484bf453dee086415e32597625733d7ef4f42852
Idan Hefetz <Idandos@users.noreply.github.com>
2018-05-10 16:16:55 +0300

    Update button_3.sh

--- mess_it_up/button_3.sh
+++ mess_it_up/button_3.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i /usr/share/httpd/.ssh/id_rsa root@overcloud-ovscompute-1.localdomain 'ifconfig eno3 down'
+ssh -i /usr/share/httpd/.ssh/id_rsa root@overcloud-ovscompute-1.localdomain 'touch /tmp/kuku2'
